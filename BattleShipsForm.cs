136ab327c7fc6407302f03bc2ff8c9ed72a05221
synackuk <53281989+synackuk@users.noreply.github.com>
2023-01-31 16:16:53 +0000

    Added main menu and rules menu

---
+++ BattleShipsForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Battleships
+{
+    public partial class BattleShipsForm : Form
+    {
+        public BattleShipsForm()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // BattleShipsForm
+            // 
+            this.ClientSize = new System.Drawing.Size(484, 561);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "BattleShipsForm";
+            this.Text = "Battleships";
+            this.ResumeLayout(false);
+            this.Controls.Add(new MainMenu());
+        }
+    }
+}
---
+++ MainMenu.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Battleships
+{
+    public partial class MainMenu : UserControl
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNewGame_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLoadGame_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRules_Click(object sender, EventArgs e)
+        {
+            Rules ruleView = new Rules();
+            this.ParentForm.Controls.Add(ruleView);
+            this.ParentForm.Controls.Remove(this);
+            this.Dispose();
+        }
+
+        private void btnScore_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.ParentForm.Close();
+        }
+    }
+}
