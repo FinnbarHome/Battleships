aee27a8dd3f4856645511e144bfdcab499ca40de
FinnbarHome <2481383@dundee.ac.uk>
2023-02-10 01:01:32 +0000

    Added a new coordinate class Fixed medium ai should be working perfectly now!!!!

--- AIPlayer.cs
+++ AIPlayer.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.PerformanceData;
+using System.Drawing.Printing;
 using System.Linq.Expressions;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ProgressBar;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
 
 namespace Battleships
@@ -16,12 +19,14 @@ namespace Battleships
         private int successX;
         private int successY;
         private int dirTries = 1;
+        private List<Coordinate> checkerboard;
 
 
         public AIPlayer(GameState gameState, Difficulty difficulty)
         {
             this.difficulty = difficulty;
             this.gameState = gameState;
+            checkerboard = GetCheckerboardCoordinates();
 
         }
 
@@ -56,9 +61,9 @@ namespace Battleships
             }
         }
 
-        public List<Tuple<int, int>> generateCheckerboardCoordinates()
+        private List<Coordinate> GetCheckerboardCoordinates()
         {
-            List<Tuple<int, int>> checkerboardCoordinates = new List<Tuple<int, int>>();
+            List<Coordinate> coordinates = new List<Coordinate>();
 
             for (int i = 0; i < 10; i++)
             {
@@ -66,17 +71,19 @@ namespace Battleships
                 {
                     if ((i + j) % 2 == 0)
                     {
-                        checkerboardCoordinates.Add(new Tuple<int, int>(i, j));
+                        Console.WriteLine(i.ToString() + j.ToString());
+                        coordinates.Add(new Coordinate(i,j));
                     }
                 }
             }
-            return checkerboardCoordinates;
+            return coordinates;
         }
+
 
         //This is the Easy AI, it hits squares completely randomly
         public void randHittingAI()
         {
-            
+
             bool finished = false;
             while (!finished)
             {
@@ -87,236 +94,258 @@ namespace Battleships
         }
 
         //Rand but for medium so it sets sucess shots
-        public void randHittingAIMed(GridSquare[][] board)
+        public Coordinate randHittingAIMed(GridSquare[][] board)
         {
-            bool finished = false;
-            while (!finished)
+            Console.WriteLine("AI STATUS: No checkered shots left to fire, doing random shots");
+            int x = rnd.Next(0, gameState.player1Square.Length);
+            int y = rnd.Next(0, gameState.player1Square.Length);
+
+            if (checkShipAtTile(board[x][y]))
             {
-                int x = rnd.Next(0, gameState.player1Square.Length);
-                int y = rnd.Next(0, gameState.player1Square.Length);
-
-                checkShipAtTile(board[x][y]);
-                if (checkShipAtTile(board[x][y]))
-                {
-                    successX = x;
-                    successY = y;
-                }
-
-                finished = gameState.hitPlayer1Square(x, y);
+                successX = x;
+                successY = y;
+                Console.WriteLine("");
+                Console.WriteLine("AI STATUS: Has hit a ship, success x:" + successX.ToString() + " y:" + successY.ToString());
             }
 
-            
+            Coordinate coordinate = new Coordinate(x, y);
+
+            return coordinate;
+        }
+
+        public Coordinate CheckeredShoot(GridSquare[][] board)
+        {
+            Console.WriteLine("AI STATUS: Trying checkered shot");
+            Coordinate selectedCoordinate = new Coordinate(-1,-1);
+
+
+            int length = checkerboard.Count;
+
+            if (length > 0)
+            {
+                Console.WriteLine("AI STATUS: Doing checkered shot");
+
+                //Generates a random index to pick from the checkboard list
+                Random rand = new Random();
+                int randomIndex = rand.Next(0, length);
+
+                //Puts this coordinate into randomCoordinate field
+                selectedCoordinate = checkerboard[randomIndex];
+                checkerboard.RemoveAt(randomIndex);
+
+                //Sets bool if a ship is going to be hit at this tile
+                if (checkShipAtTile(board[selectedCoordinate.X][selectedCoordinate.Y]))
+                {
+                    successX = selectedCoordinate.X;
+                    successY = selectedCoordinate.Y;
+                    Console.WriteLine("");
+                    Console.WriteLine("AI STATUS: Has hit a ship, success x:" + successX.ToString() + " y:" + successY.ToString());
+                }
+                return selectedCoordinate;
+            }
+            else
+            {
+                Console.WriteLine("AI STATUS: No checkered shots left to fire, doing random shots instead");
+                //If there's no checkered shots left, fire randomly
+                selectedCoordinate = randHittingAIMed(board);
+                return selectedCoordinate;
+            }
+
         }
 
         //This is the Medium AI, it shoots in a checkered pattern and hunts 
         public void checkeredHuntAI()
         {
+            Console.WriteLine("AI STATUS: Starting checkered hunt AI");
             GridSquare[][] board = gameState.player1Square;
-            GridSquare gridValue;
-            Ship ship;
-            int tryingDirX = 0;
-            int tryingDirY = 0;
-
 
             if (foundShip)
             {
+                Console.WriteLine("AI STATUS: Found a ship turn so will search this turn");
                 foundShipLastTurn = true;
             }
 
             bool finished = false;
+            int counter = 0;
+
             while (!finished)
             {
-                
-                //Generates initial random shot
-                int x = rnd.Next(0, gameState.player1Square.Length);
-                int y = rnd.Next(0, gameState.player1Square.Length);
-
-                List<Tuple<int, int>> checkerboard = generateCheckerboardCoordinates();
-                
-
+                //Stores the coordinate we're gonna try fire at
+                Coordinate coordinates = new Coordinate(-1, -1);
 
                 //Is shooting in the checkered pattern until it finds a ship
                 if (!foundShipLastTurn)
                 {
-                    int length = checkerboard.Count;
-                    if(length > 0)
-                    {
-                        //Generates a random index to pick from the checkboard list
-                        Random rand = new Random();
-                        int randomIndex = rand.Next(0, checkerboard.Count);
-
-                        //Puts this coordinate into randomCoordinate field then removes it from the checkboard
-                        Tuple<int, int> randomCoordinate = checkerboard[randomIndex];
-                        checkerboard.RemoveAt(randomIndex);
-
-                        //Get the x and y co-ordinates into separate fields
-                        x = randomCoordinate.Item1;
-                        y = randomCoordinate.Item2;
-
-                        //Sets bool if a ship is going to be hit at this tile
-                        checkShipAtTile(board[x][y]);
-                        if (checkShipAtTile(board[x][y]))
-                        {
-                            successX = x;
-                            successY = y;
-                        }
-
-                        //Try hitting this co-ord
-                    }
-                    else
-                    {
-                        finished = false;
-                        while (!finished)
-                        {
-                            randHittingAIMed(board);
-                        }
-                    }
-                    
+                    coordinates = CheckeredShoot(board);
                 }
                 else
                 {
-
-                    switch (dirTries)
-                    {
-                        case 1:
-                            //Is trying the square left of it
-                            tryingDirX = successX - 1;
-                            tryingDirY = successY;
-
-                            //If the x is beyond the grid
-                            if (tryingDirX < 0)
-                            {
-                                goto case 2;
-                            }
-                            gridValue = board[tryingDirX][tryingDirY];
-                            if (gridValue.isHit())
-                            {
-                                goto case 2;
-                            }
-
-                            x = tryingDirX;
-                            y = tryingDirY;
-                            ship = gridValue.getShip();
-
-                            if (ship != Ships.NONE)
-                            {
-                                //Has found another ship
-                                successX = tryingDirX;
-                                successY = tryingDirY;
-                            }
-                            else
-                            {
-                                dirTries++;
-                            }
-                            break;
-
-                        case 2:
-                            //Is trying the square right of it
-                            tryingDirX = successX + 1;
-                            tryingDirY = successY;
-
-                            //If the x is beyond the grid
-                            if (tryingDirX > 10)
-                            {
-                                goto case 3;
-                            }
-                            gridValue = board[tryingDirX][tryingDirY];
-                            if (gridValue.isHit())
-                            {
-                                goto case 3;
-                            }
-
-                            x = tryingDirX;
-                            y = tryingDirY;
-                            ship = gridValue.getShip();
-
-                            if (ship != Ships.NONE)
-                            {
-                                //Has found another ship
-                                successX = tryingDirX;
-                                successY = tryingDirY;
-                            }
-                            else
-                            {
-                                dirTries++;
-                            }
-                            break;
-                        case 3:
-                            //Is trying the square on the bottom of it
-                            tryingDirX = successX;
-                            tryingDirY = successY - 1;
-
-                            //If the y is beyond the grid
-                            if (tryingDirY < 0)
-                            {
-                                goto case 4;
-                            }
-                            gridValue = board[tryingDirX][tryingDirY];
-                            if (gridValue.isHit())
-                            {
-                                goto case 4;
-                            }
-
-                            x = tryingDirX;
-                            y = tryingDirY;
-                            ship = gridValue.getShip();
-
-                            if (ship != Ships.NONE)
-                            {
-                                //Has found another ship
-                                successX = tryingDirX;
-                                successY = tryingDirY;
-                            }
-                            else
-                            {
-                                dirTries++;
-                                //Has not found another ship
-                            }
-                            break;
-                        case 4:
-                            //Is trying the square on top of it
-                            tryingDirX = successX;
-                            tryingDirY = successY + 1;
-                            
-
-                            //If the y is beyond the grid
-                            if (tryingDirY > 10)
-                            {
-                                goto case 4;
-                            }
-                            gridValue = board[tryingDirX][tryingDirY];
-                            if (gridValue.isHit())
-                            {
-                                goto case 4;
-                            }
-
-                            x = tryingDirX;
-                            y = tryingDirY;
-                            ship = gridValue.getShip();
-
-                            if (ship != Ships.NONE)
-                            {
-                                //Has found another ship
-                                successX = tryingDirX;
-                                successY = tryingDirY;
-                            }
-                            else
-                            {
-                                dirTries++;
-                                //Has not found another ship
-                            }
-                            break;
-                        default:
-                            foundShip = false;
-                            dirTries = 1;
-                            randHittingAIMed(board);
-                            break;
-                    }
+                    coordinates = searchForShip(board);
                 }
-                finished = gameState.hitPlayer1Square(x, y);
+                
+                counter++;
+                Console.WriteLine("AI STATUS: Has looped through trying to shoot this many times: " + counter.ToString());
+                Console.WriteLine("AI STATUS: firing at X:" + coordinates.X.ToString() + " Y:" + coordinates.Y.ToString());
+                finished = gameState.hitPlayer1Square(coordinates.X, coordinates.Y);
             }
 
 
+        }
+
+        public Coordinate searchForShip(GridSquare[][] board)
+        {
+            Coordinate coordinates = new Coordinate(-1, -1);
+            int tryingDirX = -1;
+            int tryingDirY = -1;
+            GridSquare gridValue;
+            Ship ship;
+
+            Console.WriteLine("AI STATUS: Trying search");
+            switch (dirTries)
+            {
+
+                case 1:
+                    //Is trying the square left of it
+                    tryingDirX = successX - 1;
+                    tryingDirY = successY;
+
+                    //If the x is beyond the grid
+                    if (tryingDirX < 0)
+                    {
+                        goto case 2;
+                    }
+                    gridValue = board[tryingDirX][tryingDirY];
+                    if (gridValue.isHit())
+                    {
+                        goto case 2;
+                    }
+
+                    ship = gridValue.getShip();
+                    if (ship != Ships.NONE)
+                    {
+                        //Has found another ship
+                        successX = tryingDirX;
+                        successY = tryingDirY;
+                        Console.WriteLine("");
+                        Console.WriteLine("AI STATUS: Has hit a ship, success x:" + successX.ToString() + " y:" + successY.ToString());
+                    }
+                    else
+                    {
+                        dirTries++;
+                    }
+
+                    coordinates.X = tryingDirX;
+                    coordinates.Y = tryingDirY;
+                    return coordinates;
+
+                case 2:
+                    //Is trying the square right of it
+                    tryingDirX = successX + 1;
+                    tryingDirY = successY;
+
+                    //If the x is beyond the grid
+                    if (tryingDirX >= 10)
+                    {
+                        goto case 3;
+                    }
+                    gridValue = board[tryingDirX][tryingDirY];
+                    if (gridValue.isHit())
+                    {
+                        goto case 3;
+                    }
+
+                    ship = gridValue.getShip();
+                    if (ship != Ships.NONE)
+                    {
+                        //Has found another ship
+                        successX = tryingDirX;
+                        successY = tryingDirY;
+                        Console.WriteLine("");
+                        Console.WriteLine("AI STATUS: Has hit a ship, success x:" + successX.ToString() + " y:" + successY.ToString());
+                    }
+                    else
+                    {
+                        dirTries++;
+                    }
+
+                    coordinates.X = tryingDirX;
+                    coordinates.Y = tryingDirY;
+                    return coordinates;
+
+                case 3:
+                    //Is trying the square on the bottom of it
+                    tryingDirX = successX;
+                    tryingDirY = successY - 1;
+
+                    //If the y is beyond the grid
+                    if (tryingDirY < 0)
+                    {
+                        goto case 4;
+                    }
+                    gridValue = board[tryingDirX][tryingDirY];
+                    if (gridValue.isHit())
+                    {
+                        goto case 4;
+                    }
+
+                    ship = gridValue.getShip();
+                    if (ship != Ships.NONE)
+                    {
+                        //Has found another ship
+                        successX = tryingDirX;
+                        successY = tryingDirY;
+                        Console.WriteLine("");
+                        Console.WriteLine("AI STATUS: Has hit a ship, success x:" + successX.ToString() + " y:" + successY.ToString());
+                    }
+                    else
+                    {
+                        dirTries++;
+                    }
+
+                    coordinates.X = tryingDirX;
+                    coordinates.Y = tryingDirY;
+                    return coordinates;
+
+                case 4:
+                    //Is trying the square on top of it
+                    tryingDirX = successX;
+                    tryingDirY = successY + 1;
+
+
+                    //If the y is beyond the grid
+                    if (tryingDirY >= 10)
+                    {
+                        goto default;
+                    }
+                    gridValue = board[tryingDirX][tryingDirY];
+                    if (gridValue.isHit())
+                    {
+                        goto default;
+                    }
+
+                    ship = gridValue.getShip();
+                    if (ship != Ships.NONE)
+                    {
+                        //Has found another ship
+                        successX = tryingDirX;
+                        successY = tryingDirY;
+                        Console.WriteLine("");
+                        Console.WriteLine("AI STATUS: Has hit a ship, success x:" + successX.ToString() + " y:" + successY.ToString());
+                    }
+                    else
+                    {
+                        dirTries++;
+                    }
+
+                    coordinates.X = tryingDirX;
+                    coordinates.Y = tryingDirY;
+                    return coordinates;
+
+                default:
+                    foundShip = false;
+                    dirTries = 1;
+                    return coordinates = randHittingAIMed(board);
+            }
         }
 
         public bool checkShipAtTile(GridSquare gridValue)
---
+++ Coordinate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Configuration;
+using System.Runtime.InteropServices;
+
+namespace Battleships
+{
+    public class Coordinate
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Coordinate(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
